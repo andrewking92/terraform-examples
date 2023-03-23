da7bb2f2dd6e688f0a4c5a154c2124c594087696
Andrew King <kingan916@gmail.com>
2023-03-23 17:25:43 +0000

    version fix

--- 01-encryption-at-rest/versions.tf
+++ 01-encryption-at-rest/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
-      version = "1.8.1-pre1"
+      version = "1.8.1"
     }
     aws = {
       source  = "hashicorp/aws"
